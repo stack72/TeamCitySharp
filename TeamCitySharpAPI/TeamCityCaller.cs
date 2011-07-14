404ae886e0f27fb17cff81d7401e922d720fe8cd
Paul Stack <paulstack@hotmail.com>
2011-07-14 21:20:55 +0100

    removal of unused usings

--- TeamCitySharpAPI/TeamCityCaller.cs
+++ TeamCitySharpAPI/TeamCityCaller.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Globalization;
-using System.IO;
-using System.Net;
-using System.Text;
 using EasyHttp.Http;
 using EasyHttp.Infrastructure;
 using TeamCitySharpAPI.DomainEntities;
