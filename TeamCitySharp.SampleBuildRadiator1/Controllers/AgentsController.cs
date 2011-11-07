8739b36f684cf4c82de5d11fdc43dae341e49932
Paul Stack <public@paulstack.co.uk>
2011-11-07 23:10:44 +0000

    starting to add the sample mvc application to show a build radiator

---
+++ TeamCitySharp.SampleBuildRadiator1/Controllers/AgentsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TeamCitySharp.SampleBuildRadiator.Controllers
+{
+    public class AgentsController : Controller
+    {
+        private TeamCityClient _client;
+        public ActionResult Index()
+        {
+            _client = new TeamCityClient("localhost:81");
+            _client.Connect("admin", "qwerty");
+            var agents = _client.AllAgents();
+
+            return View(agents);
+        }
+
+    }
+}
---
+++ TeamCitySharp.SampleBuildRadiator1/Controllers/ProjectsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace TeamCitySharp.SampleBuildRadiator.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        private TeamCityClient _client;
+        public ActionResult Index()
+        {
+            _client = new TeamCityClient("localhost:81");
+            _client.Connect("admin", "qwerty");
+            var projects = _client.AllProjects();
+
+            return View(projects);
+        }
+
+    }
+}
