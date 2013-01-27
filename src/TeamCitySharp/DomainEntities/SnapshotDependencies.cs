a09a988bf0218e06e6fcdfe709a4640ce88cb074
Serge Baltic <baltic@hypersw.net>
2013-01-28 01:56:11 +0400

    Add parsing of Snapshot Dependencies list when fetching a build type.

---
+++ src/TeamCitySharp/DomainEntities/SnapshotDependencies.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace TeamCitySharp.DomainEntities
+{
+	public class SnapshotDependencies
+	{
+		public override string ToString()
+		{
+			return "snapshot-dependencies";
+		}
+
+		public List<SnapshotDependency> SnapshotDependency { get; set; }
+	}
+}
