4620e02bbc561a4cd4fe74ae8c85d3ceb0ba65f6
Nicolai Heilbuth <nheilbuth@hotmail.com>
2014-01-07 12:27:55 +0100

    Changed Json encoder to use lowercase

--- src/TeamCitySharp/Connection/TeamcityJsonEncoderDecoderConfiguration.cs
+++ src/TeamCitySharp/Connection/TeamcityJsonEncoderDecoderConfiguration.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using EasyHttp.Codecs;
 using EasyHttp.Codecs.JsonFXExtensions;
 using EasyHttp.Configuration;
+using JsonFx.IO;
 using JsonFx.Json;
+using JsonFx.Model;
 using JsonFx.Serialization;
+using JsonFx.Serialization.Filters;
 
 namespace TeamCitySharp.Connection
 {
@@ -11,7 +15,7 @@ namespace TeamCitySharp.Connection
     {
         public IEncoder GetEncoder()
         {
-            var jsonWriter = new JsonWriter(new DataWriterSettings(DefaultEncoderDecoderConfiguration.CombinedResolverStrategy()
+            var jsonWriter = new LowerCaseJsonWriter(new DataWriterSettings(DefaultEncoderDecoderConfiguration.CombinedResolverStrategy()
                 , new TeamCityDateFilter()), new[] { "application/.*json", "text/.*json" });
 
             var writers = new List<IDataWriter> { jsonWriter };
@@ -27,6 +31,28 @@ namespace TeamCitySharp.Connection
             var readers = new List<IDataReader> { jsonReader };
             var dataReaderProvider = new RegExBasedDataReaderProvider(readers);
             return new DefaultDecoder(dataReaderProvider);
+        }
+    }
+
+    public class LowerCaseJsonWriter : JsonWriter
+    {
+        public LowerCaseJsonWriter(DataWriterSettings settings, params string[] contentTypes):base(settings, contentTypes)
+        {}
+
+        protected override ITextFormatter<ModelTokenType> GetFormatter()
+        {
+            return new LowerCaseJsonFormatter(this.Settings);
+        }
+    }
+
+    public class LowerCaseJsonFormatter : JsonWriter.JsonFormatter
+    {
+        public LowerCaseJsonFormatter(DataWriterSettings settings) : base(settings)
+        {}
+
+        protected override void WritePropertyName(TextWriter writer, string propertyName)
+        {
+            base.WritePropertyName(writer, propertyName.ToLower());
         }
     }
 }
