cefd904f1779bdf5329c6b3bb59b6d6a2bf51029
Ron <tikicoder@users.noreply.github.com>
2014-09-09 10:40:23 -0700

    Update IChanges.cs
    updated to allow custom http timeout.

--- src/TeamCitySharp/ActionTypes/IChanges.cs
+++ src/TeamCitySharp/ActionTypes/IChanges.cs
@@ -5,9 +5,11 @@ namespace TeamCitySharp.ActionTypes
 {
     public interface IChanges
     {
-        List<Change> All();
-        Change ByChangeId(string id);
-        Change LastChangeDetailByBuildConfigId(string buildConfigId);
-        List<Change> ByBuildConfigId(string buildConfigId);
+        List<Change> All(int aHttpTimeOut = -1);
+        Change ByChangeId(string id, int aHttpTimeOut = -1);
+        Change LastChangeDetailByBuildConfigId(string buildConfigId, int aHttpTimeOut = -1);
+        List<Change> ByBuildConfigId(string buildConfigId, int aHttpTimeOut = -1);
+        List<Change> ByBuild(Build aBuild, int aHttpTimeOut = -1);
+        List<Change> ByBuildId(int aBuildId, int aHttpTimeOut = -1);
     }
 }
