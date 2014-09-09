29a1727a940066c47de04ecc49721cad603149dd
Ron <tikicoder@users.noreply.github.com>
2014-09-09 10:39:48 -0700

    Update Changes.cs
    updated code to allow HttpTimeOut Request to be customized.

--- src/TeamCitySharp/ActionTypes/Changes.cs
+++ src/TeamCitySharp/ActionTypes/Changes.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using TeamCitySharp.Connection;
 using TeamCitySharp.DomainEntities;
@@ -14,32 +14,57 @@ namespace TeamCitySharp.ActionTypes
             _caller = caller;
         }
 
-        public List<Change> All()
+        public List<Change> All(int aHttpTimeOut=-1)
         {
+            if (aHttpTimeOut > -1)
+                _caller.httpTimeOut = aHttpTimeOut;
             var changeWrapper = _caller.Get<ChangeWrapper>("/app/rest/changes");
 
             return changeWrapper.Change;
         }
 
-        public Change ByChangeId(string id)
+        public Change ByChangeId(string id, int aHttpTimeOut = -1)
         {
+            if (aHttpTimeOut > -1)
+                _caller.httpTimeOut = aHttpTimeOut;
             var change = _caller.GetFormat<Change>("/app/rest/changes/id:{0}", id);
 
             return change;
         }
 
-        public List<Change> ByBuildConfigId(string buildConfigId)
+        public List<Change> ByBuildConfigId(string buildConfigId, int aHttpTimeOut = -1)
         {
+            if (aHttpTimeOut > -1)
+                _caller.httpTimeOut = aHttpTimeOut;
             var changeWrapper = _caller.GetFormat<ChangeWrapper>("/app/rest/changes?buildType={0}", buildConfigId);
 
             return changeWrapper.Change;
         }
 
-        public Change LastChangeDetailByBuildConfigId(string buildConfigId)
+        public Change LastChangeDetailByBuildConfigId(string buildConfigId, int aHttpTimeOut = -1)
         {
+            if (aHttpTimeOut > -1)
+                _caller.httpTimeOut = aHttpTimeOut;
             var changes = ByBuildConfigId(buildConfigId);
 
             return changes.FirstOrDefault();
+        }
+
+        public List<Change> ByBuild(Build aBuild, int aHttpTimeOut = -1)
+        {
+            int buildId;
+            if (!int.TryParse(aBuild.Id, out buildId))
+                buildId = -1;
+            return ByBuildId(buildId, aHttpTimeOut);
+        }
+
+        public List<Change> ByBuildId(int aBuildId, int aHttpTimeOut = -1)
+        {
+            if (aHttpTimeOut > -1)
+                _caller.httpTimeOut = aHttpTimeOut;
+            var changeWrapper = _caller.GetFormat<ChangeWrapper>("/app/rest/changes?build=id:{0}", aBuildId);
+
+            return changeWrapper.Change;
         }
 
     }
