9ac1d31d26565da8ce38906c9c5c7ee5963c3d64
Alan NULL <alan-null@outlook.com>
2015-06-05 00:04:33 +0200

    Added support for Cruise Control https://confluence.jetbrains.com/display/TW/REST+API#RESTAPI-CCTray

---
+++ src/TeamCitySharp/DomainEntities/CruiseControlTray/Project.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TeamCitySharp.DomainEntities.CCTray
+{
+    public class Project
+    {
+        public string Activity { get; set; }
+        public int LastBuildLabel { get; set; }
+        public string LastBuildStatus { get; set; }
+        public DateTime LastBuildTime { get; set; }
+        public string Name { get; set; }
+        public string WebUrl { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ src/TeamCitySharp/DomainEntities/CruiseControlTray/ProjectWrapper.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace TeamCitySharp.DomainEntities.CCTray
+{
+    public class ProjectWrapper
+    {
+        public List<Project> Project { get; set; }
+    }
+}
